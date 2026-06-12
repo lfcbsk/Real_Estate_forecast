e0721c9c7f097f59c83a7c8d6fc967b0de97c0a2
lfcbsk <165536191+lfcbsk@users.noreply.github.com>
2026-06-12 15:44:22 +0700

    fix lint: code format

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,9 +1,14 @@
 from pathlib import Path
+
 import yaml
 
 
 def load_config():
-    config_path = Path(__file__).resolve().parents[2] / "configs" / "config.yaml"
+    config_path = (
+        Path(__file__).resolve().parents[2]
+        / "configs"
+        / "config.yaml"
+    )
+
     with open(config_path, "r") as f:
         return yaml.safe_load(f)
-    
